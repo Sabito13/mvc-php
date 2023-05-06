50217e8592ba45619bc800e962d7f774893e51f0
Esteban <esabinio13@gmail.com>
2023-05-06 20:43:11 -0300

    first commit

---
+++ information.php
@@ -0,0 +1,4 @@
+<?php 
+$arrayWithData = array(1 => 'test one',
+ 2 => 'test two',);
+?>
---
+++ main.php
@@ -0,0 +1,19 @@
+<?php include('information.php'); ?>
+
+
+<?php include('includes/header.php')?>
+  <div>hello world from div 1</div>
+  <?php echo "hello from php"?>
+  <div>hello world from div 2</div>
+  <?php $newHello = "hello from variable"?>
+  <div>hello world from div 3</div>
+  <?php echo $newHello?>
+  <br>
+  <?php echo $arrayWithData[2]?>
+  
+  
+  
+  
+
+</body>
+</html>
