3d4c6ada733524ef5dbe3fd269f825c1539d0303
Esteban <esabinio13@gmail.com>
2023-05-06 21:24:09 -0300

    conexion con la base de datos

--- information.php
+++ information.php
@@ -1,4 +1,9 @@
 <?php 
 $arrayWithData = array(1 => 'test one',
  2 => 'test two',);
+
+ $materias = array(
+  01017 => ["alg y geometria","sin correlativas"],
+  01019 => ["alg y geometria","01017"]
+);
 ?>
--- main.php
+++ main.php
@@ -1,4 +1,5 @@
 <?php include('information.php'); ?>
+<?php include('db_conexion.php'); ?>
 
 
 <?php include('includes/header.php')?>
@@ -9,11 +10,33 @@
   <div>hello world from div 3</div>
   <?php echo $newHello?>
   <br>
-  <?php echo $arrayWithData[2]?>
-  
-  
+  <?php echo $arrayWithData[2];?>
+  <br>
+  <?php echo implode($arrayWithData)." \n"; ?>
+  <br>
+  <?php echo implode("----",$arrayWithData);?> 
+  <br>
+  <?php echo implode("----", $materias[01017]); ?>
+  <br>
+  <?php echo implode("----", $materias[01019]); ?>
+  <br>
+  <?php
+  $query = "INSERT INTO materias(id_materia, nom_materia,correlativas) VALUES (01015,'algebra', 'no')";
+  $result = mysqli_query($connexion_db, $query);
+  if(!$result) {
+    die("Query Failed.");
+  }
   
   
 
+  $query = "SELECT * FROM materias";
+  $result_materias = mysqli_query($connexion_db, $query);    
+  while($row = mysqli_fetch_assoc($result_materias)) { ?>
+          <tr>
+            <td><?php echo $row['id_materia']; ?></td>
+            <td><?php echo $row['nom_materia']; ?></td>
+            <td><?php echo $row['correlativas']; ?></td>
+            </tr>
+<?php } ?>
 </body>
 </html>
