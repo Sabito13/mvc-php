1ed92a076c10f9bc65ea80a0ced285787caf4150
Esteban <83845301+Sabito13@users.noreply.github.com>
2023-06-09 13:10:36 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,11 @@
 <?php
 /* Valores constatantes para la conenxion con la base de datos */
-define("DB_HOST", "localhost");
-define("DB", 'php_materias_crud');
+define("DB_HOST", "containers-us-west-147.railway.app:7846");
+define("DB", 'railway');
 define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_PASS", "cjnaPPq47Fd6liYJOq9W");
+define("DB_PORT", "7846");
+
 
 
 
@@ -13,5 +15,5 @@ define("DB_PASS", "");
 define('BASE_DIR', __DIR__);
 
 /** Ruta en el navegador */
-define('BASE_URL', 'http://localhost/crud-materias');
+define('BASE_URL', 'https://mvc-php-production.up.railway.app');
 ?>
