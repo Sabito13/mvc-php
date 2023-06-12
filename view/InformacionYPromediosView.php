5884d542814578da1199fcedf2fdd59580698465
Esteban <83845301+Sabito13@users.noreply.github.com>
2023-06-12 12:47:38 -0300

    Update InformacionYPromediosView.php

--- view/InformacionYPromediosView.php
+++ view/InformacionYPromediosView.php
@@ -15,8 +15,14 @@
   } 
   ?>
   
-  <div class="materia-promedio">Nota promedio => <?php echo $nota_total/$contador_materias;?></div>
-  <div class="materia-mayor">Materias con nota mayor o igual a 7 => <?php echo $contador_materias_mas_siete;?></div>
+  <div class="materia-promedio-container"> 
+    <div  class="materia-promedio-estatico">Nota promedio</div>
+      <div class="materia-promedio-dinamico"><?php echo $nota_total/$contador_materias;?></div>
+  </div>
+  <div class="materia-mayor-container">
+    <div class="materia-mayor-estatico"> Materias con nota mayor o igual a 7 </div>
+    <div class="materia-mayor-dinamico"><?php echo $contador_materias_mas_siete;?></div>
+   </div>
 
 
 </div>
