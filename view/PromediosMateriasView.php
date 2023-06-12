a59195f8931c8c20d7d2503830e6c404f298b334
Esteban <83845301+Sabito13@users.noreply.github.com>
2023-06-12 11:50:49 -0300

    Update PromediosMateriasView.php

--- view/PromediosMateriasView.php
+++ view/PromediosMateriasView.php
@@ -1,1 +1,8 @@
-
+<div class="contanainer-promedio">
+    <form class="form" action=<?php echo constant('BASE_URL')."/request.php"?> method="GET">
+        <h3>Promedio del alumno</h3>
+        <div class="promedio-materia">
+           <input type="submit" value="Promedio" name="crud-action"/>
+        </div>
+    </form>
+ </div>
